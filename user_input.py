3c5cd98bb21b48b7bba24b3ae17613312a5b139d
Tonnymunene <149967772+Tonnymunene@users.noreply.github.com>
2024-03-22 23:53:48 +0300

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,4 @@
+name=input("What is your name \n")
+age=input("How old are?\n")
+location=input("Where are you located?\n")
+print("Hello"+" "+(name),"you are"+" "+(str(age))+" "+"years old"+" "+"and you live in"+" "+(location))
